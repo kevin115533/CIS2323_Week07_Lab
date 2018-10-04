ff047c5229c0e2d28c08b5f00d222b7805ed2707
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2018-10-03 20:07:28 -0500

    got things working

--- Dog.java
+++ Dog.java
@@ -1,33 +1,13 @@
 public class Dog extends Canine{
-	private int height;
 	private String breed;
-	boolean isDomesticated;
-	
-	public Dog(){
-		breed = null;
-		isDomesticated = true;
-	}
 	
 	public Dog(String x){
 		setBreed(x);
 		isDomesticated = true;
 	}
-	
-	public void setHeight(int x){
-		height = x;
-	}
-	
-	public int getHeight(){
-		return height;
-	}
-	
+
 	public void setBreed(String x){
 		breed = x;
 	}
-	
-	public String getBreed(){
-		return breed;
-	}
-	
-	
+
 }
